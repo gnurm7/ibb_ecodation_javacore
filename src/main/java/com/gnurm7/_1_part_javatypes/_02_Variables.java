dc3bed98ca6e783b43ce0b0a4378d007b51eea9b
gülsüm nur <gulsum12nur@gmail.com>
2025-02-19 01:57:22 +0300

    Data variables,names rules

---
+++ src/main/java/com/gnurm7/_1_part_javatypes/_02_Variables.java
@@ -0,0 +1,24 @@
+package com.gnurm7._1_part_javatypes;
+
+public class _02_Variables {
+    //veri değişken isimlerini yazarken;
+/*
+veri isimlendirmeleri;
+1-) isim veya sıfat ,zamir kullanmamız lazım.
+2-)sayı ile bitebilir .
+3-)sayı ile başlanmaz.
+4-)_ veya $ dışında özel simgelerle tanımlanmaz
+5-) _ veya $ile başlayabilirsiniz
+6-) Değişken isimleri camelCase kuralına göre yazılır.
+7-)Anlamlı ve açıklayıcı isimler seçiniz.
+*/
+
+
+    public static void main(String[] args) {
+        int schoolNumber=23;
+        System.out.println(schoolNumber);
+
+
+
+    }
+}
---
+++ src/main/java/com/gnurm7/_1_part_javatypes/_03_JavaPrimitiveTypes.java
@@ -0,0 +1,29 @@
+package com.gnurm7._1_part_javatypes;
+
+public class _03_JavaPrimitiveTypes {
+
+    public static void main(String[] args) {
+        byte b =123;
+        short s1=12345;
+        int i=1133444342;
+        long l1=2342134342342323223L;
+        float f2=3.134f;
+        double d2=3.24553;
+        boolean b3= true;
+        char c1='a';
+        System.out.println("byte :"+b);
+        System.out.println("short :"+s1);
+        System.out.println("int :"+i);
+        System.out.println("long :"+l1);
+        System.out.println("float :"+f2);
+        System.out.println("double :"+d2);
+        System.out.println("boolean :"+b3);
+        System.out.println("char :"+c1);
+
+
+
+
+
+
+    }
+}
