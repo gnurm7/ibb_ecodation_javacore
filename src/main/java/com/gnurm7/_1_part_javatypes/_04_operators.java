b981e53ea48ae83e223f2664d4fb1e1e7e93e643
gülsüm nur <gulsum12nur@gmail.com>
2025-02-20 01:35:16 +0300

    operators

---
+++ src/main/java/com/gnurm7/_1_part_javatypes/_04_operators.java
@@ -0,0 +1,33 @@
+package com.gnurm7._1_part_javatypes;
+
+
+
+public class _04_operators {
+    public static void main(String[] args) {
+        //operator
+        /*
+        * +
+        * -
+        * *
+        * /
+        * %
+        * ++
+        * --
+        */
+
+        int number1=123, number2=14;
+        int add= number1+number2;
+        int sub= number1-number2;
+        int mul= number1*number2;
+        int div= number1/number2;
+        int modulus= number1%number2;
+        System.out.println("toplama: "+add);
+        System.out.println("çıkarma: "+sub);
+        System.out.println("çarpma: "+mul);
+        System.out.println("bölme: "+div);
+        System.out.println("kalan: "+modulus);
+        // Mantıksal Operators: &&=VE ||=VEYA !=değil
+        // ++ 1 artır,  -- 1 azalt
+
+    }
+}
